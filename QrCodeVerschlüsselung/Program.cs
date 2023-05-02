836fc3ff95a1f1944949698a031b6af6f7a99e0e
m.nikbakht <m.nikbakht@INFARDA005>
2023-05-02 10:36:42 +0330

    Add project files.

---
+++ QrCodeVerschlüsselung/Program.cs
@@ -0,0 +1,25 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QrCodeVerschlüsselung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var str = "https://github.com/mahdiNik";
+            var path = @".\output.jpg";
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(str, QRCodeGenerator.ECCLevel.Q);
+            BitmapByteQRCode qrCode = new BitmapByteQRCode(qrCodeData);
+            byte[] qrCodeAsBitmapByteArr = qrCode.GetGraphic(20);
+            File.WriteAllBytes(path, qrCodeAsBitmapByteArr);
+        }
+    }
+}
